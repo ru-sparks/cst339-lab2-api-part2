85e48fdb02d28125733a8c6a83863318903784c6
James Sparks <ru_sparks@yahoo.com>
2026-06-18 11:55:01 -0700

    Initial commit

---
+++ src/main/java/com/sparkco/lab2_part2_entity_generator/Lab2Part2EntityGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sparkco.lab2_part2_entity_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Part2EntityGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Part2EntityGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparkco/lab2_part2_entity_generator/Lab2Part2EntityGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkco.lab2_part2_entity_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Part2EntityGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
