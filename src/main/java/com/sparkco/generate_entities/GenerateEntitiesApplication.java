8139f8a8cdcea2a6a0bdbd53ed8139625b69d68e
James Sparks <ru_sparks@yahoo.com>
2026-06-18 16:53:16 -0700

    use JDBC not JPA

--- src/main/java/com/sparkco/generate_entities/GenerateEntitiesApplication.java
+++ src/main/java/com/sparkco/generate_entities/GenerateEntitiesApplication.java
@@ -1,13 +1,13 @@
-package com.sparkco.lab2_part2_entity_generator;
+package com.sparkco.generate_entities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab2Part2EntityGeneratorApplication {
+public class GenerateEntitiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab2Part2EntityGeneratorApplication.class, args);
+		SpringApplication.run(GenerateEntitiesApplication.class, args);
 	}
 
 }
